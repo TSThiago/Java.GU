53306b357836acd50511cfe3983fd5ed51a0cbb0
Thiago Shibanuma <thiago.tshibanuma@gmail.com>
2021-06-05 23:15:45 -0300

    Adição do Programa06

---
+++ Programa06.java
@@ -0,0 +1,25 @@
+package geek;
+
+//Instrução Switch
+public class Programa06 {
+	public static void main(String[] args) {
+		int numero = 33;
+		
+		switch (numero) {
+			case 1:
+				System.out.println("O número é 1");
+				break;
+			case 3:
+				System.out.println("O número é 3");
+				break;
+			case 5:
+				System.out.println("O número é 5");
+				break;
+			//Opcional, importante quando não há nenhum dos casos 
+			default:
+				System.out.println("[Default] O número é " + numero);
+				break;
+		}
+	
+}
+}
